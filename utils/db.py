fe769ad749fd1c61b361a8b5117fb99520bccef0
yezi <yehaowei20060411@qq.com>
2023-01-01 05:58:01 +0800

    feat: 完善基础设施

---
+++ utils/db.py
@@ -0,0 +1,22 @@
+from pymongo import IndexModel, MongoClient
+
+from utils.config import config
+
+
+def init_DB(db_name: str):
+    connection: MongoClient = MongoClient(config.db.host, config.db.port)
+    db = connection[db_name]
+    return db
+
+
+db = init_DB(config.db.main_database)
+
+run_log_db = db.run_log
+access_log_db = db.access_log
+user_data_db = db.user_data
+timeline_data_db = db.timeline_data
+
+
+# 创建索引
+
+# TODO
