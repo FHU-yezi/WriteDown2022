26d4f74fd815c239d739de738a1875901a9d74aa
yezi <yehaowei20060411@qq.com>
2023-04-26 06:16:25 +0800

    fix: 修复 Cookie 操作相关的问题

--- utils/page.py
+++ utils/page.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, Optional, Set
 
 from pywebio.session import eval_js, info
-from sspeedup.pywebio.cookies import get_cookies, remove_cookies, set_cookies
+from sspeedup.pywebio.cookies import get_cookies, remove_cookie, set_cookie
 from sspeedup.pywebio.navigation import get_base_url
 
 URL_SCHEME_ALLOW_LIST: Set[str] = {"Android", "iPhone", "iPad"}
@@ -22,14 +22,14 @@ def get_jump_link(module_name: str, query_args: Optional[Dict[str, Any]] = None)
 
 
 def set_user_slug_cookies(user_slug: str) -> None:
-    set_cookies({"user_slug": user_slug})
+    set_cookie(key="user_slug", value=user_slug, max_age=2592000, secure=True)
 
 
 def get_user_slug_cookies() -> Optional[str]:
     return get_cookies().get("user_slug")
 
 def remove_user_slug_cookies() -> None:
-    remove_cookies()
+    remove_cookie("user_slug")
 
 def is_full_width() -> bool:
     return eval_js('$("#output-container")[0].clientWidth === 880')  # type: ignore
